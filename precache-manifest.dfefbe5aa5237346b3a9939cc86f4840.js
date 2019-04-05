b50f42717e12ff82aadfa6f0c94b096546cfbb77
Michał Wrzosek <michal@wrzosek.pl>
2019-04-05 20:37:10 +0200

    Updates

---
+++ precache-manifest.dfefbe5aa5237346b3a9939cc86f4840.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d069244224dcfed9c2d",
+    "url": "/deep-names/static/js/runtime~main.6d069244.js"
+  },
+  {
+    "revision": "ee71ed1bc8e97b2423d6",
+    "url": "/deep-names/static/js/main.ee71ed1b.chunk.js"
+  },
+  {
+    "revision": "a851e43c7a08f87dc6b5",
+    "url": "/deep-names/static/js/1.a851e43c.chunk.js"
+  },
+  {
+    "revision": "ee71ed1bc8e97b2423d6",
+    "url": "/deep-names/static/css/main.a5de8ccc.chunk.css"
+  },
+  {
+    "revision": "700f689f97c32167929f04ef59fc100b",
+    "url": "/deep-names/index.html"
+  }
+];
